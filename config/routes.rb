e628c4029768e82a26366ce777d28842ff4801c1
Alvaro Muhlethaler <amuhlethaler@vairix.com>
2012-10-26 12:06:32 -0200

    Root point to Orders

--- config/routes.rb
+++ config/routes.rb
@@ -68,7 +68,7 @@ Delivery::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'clients#index'
+  root :to => 'orders#index'
 
   # See how all your routes lay out with "rake routes"
 
