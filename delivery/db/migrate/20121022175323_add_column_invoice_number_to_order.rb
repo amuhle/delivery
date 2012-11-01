3e5fbbe5e02e1464d50add8b14aca271b2265366
Alvaro Muhlethaler <amuhlethaler@vairix.com>
2012-11-01 19:07:02 -0200

    Revert "Revert "removes delivery folder""
    This reverts commit 2b9e6d9442385615eb74866d84c5c21d592e0126.

--- delivery/db/migrate/20121022175323_add_column_invoice_number_to_order.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnInvoiceNumberToOrder < ActiveRecord::Migration
-  def change
-    add_column :orders, :invoice_number, :integer
-  end
-end
--- delivery/db/migrate/20121029140243_change_column_active_to_ordres.rb
+++
@@ -1,9 +0,0 @@
-class ChangeColumnActiveToOrdres < ActiveRecord::Migration
-  def up
-    change_column :orders, :active, :boolean, default: true
-  end
-
-  def down
-    change_column :orders, :active, :bollean, default: true
-  end
-end
