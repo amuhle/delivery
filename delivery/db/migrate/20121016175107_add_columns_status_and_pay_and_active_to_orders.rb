e9fb801570cad105849b0a410c60f6cdffa1b575
Alvaro Muhlethaler <amuhlethaler@vairix.com>
2012-11-01 18:48:55 -0200

    Revert "Revert "removes delivery folder""
    This reverts commit dc437e3b3529318da44202e78b1b5b58568cea6b.

--- delivery/db/migrate/20121016175107_add_columns_status_and_pay_and_active_to_orders.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnsStatusAndPayAndActiveToOrders < ActiveRecord::Migration
-  def change
-    add_column :orders, :status, :string, default: 'Pending'
-    add_column :orders, :pay, :boolean, default: false
-    add_column :orders, :active, :boolean, default: false
-  end
-end
