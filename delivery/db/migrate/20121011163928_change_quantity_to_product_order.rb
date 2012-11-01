419f9bbf98d8d852662cbbb07d0a52f31456fa47
Alvaro Muhlethaler <amuhlethaler@vairix.com>
2012-11-01 19:07:04 -0200

    Revert "Revert "Revert "test"""
    This reverts commit 5d4e4cfc40e349abaebadd64870aae0b7f3dbdf4.

--- delivery/db/migrate/20121011163928_change_quantity_to_product_order.rb
+++
@@ -1,9 +0,0 @@
-class ChangeQuantityToProductOrder < ActiveRecord::Migration
-  def up
-    change_column :products_orders, :quantity, :decimal
-  end
-
-  def down
-    add_column :prducts_orders, :quantity, :integer
-  end
-end
--- delivery/db/migrate/20121012135216_change_total_to_order.rb
+++
@@ -1,9 +0,0 @@
-class ChangeTotalToOrder < ActiveRecord::Migration
-  def up
-    change_column :orders, :total, :decimal
-  end
-
-  def down
-    change_column :orders, :total, :decimal
-  end
-end
