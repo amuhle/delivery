b8e8acfef9e59b2432f969c7e6590d2385de72a8
Toto <psaintgirons@vairix.com>
2012-11-01 17:56:28 -0200

    added admin profile

--- app/helpers/orders_helper.rb
+++
@@ -1,9 +0,0 @@
-module OrdersHelper
-  def autocomplete(text_field_sym, elements, options = {})
-    # text_field_name => text_field_sym
-    # options
-    # :show => 'show_value'
-    # :value => 'value'
-    # :change_event => {:price_dom_element => :price_field_from_elements}
-  end
-end
--- db/migrate/20120901134515_add_rut_and_contact_to_clients.rb
+++
@@ -1,6 +0,0 @@
-class AddRutAndContactToClients < ActiveRecord::Migration
-  def change
-    add_column :clients, :rut, :string
-    add_column :clients, :contact, :string
-  end
-end
